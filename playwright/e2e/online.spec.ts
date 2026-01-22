53131afe9adb071aaa036af5aec0e3d02cbc4d3d
Dany <dbbatista@gmail.com>
2026-01-22 12:47:42 -0700

    my first commit

---
+++ playwright/e2e/online.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test'
+
+test('webapp should be online', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
+
+  await expect(page).toHaveTitle(/Velô by Papito/)
+})
+
+
